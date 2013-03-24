c8b7064ad7bb419439f7ee2c88c322d20fa4d5a4
MichaelKing <michael.king.uk@googlemail.com>
2013-03-24 17:56:32 +0000

    Existing files that are changed.

--- CaptainAhab.cpp
+++ CaptainAhab.cpp
@@ -5,35 +5,12 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
-#include <iostream>
-#include <cassert>
-#define LIBAIFF_NOCOMPAT 1
-extern "C"
-{
-#include <libaiff/libaiff.h>
-}
-#include <fftw3.h>
-using namespace std;
+#include "WhaleSoundRecognition.h"
 
 int main() {
-	AIFF_Ref ref = AIFF_OpenFile("/home/michael/Documents/Kaggle/Whale sounds/data/train/train1.aiff", F_RDONLY);
-
-	// Our AIFF files have audio format LPCM and 16 bits per sample.
-	int16_t * samples = new int16_t[ref->nSamples];
-	int numBytesRead = AIFF_ReadSamples(ref,samples,ref->nSamples*ref->bitsPerSample/8) ;
-	assert(numBytesRead == ref->nSamples*ref->bitsPerSample/8);
-	// Convert to doubles to use with the fftw
-	double * samplesD = new double[ref->nSamples];
-	for (size_t i=0; i<ref->nSamples; ++i)
-	{
-		samplesD[i] = (double)samples[i];
-	}
-	fftw_complex * out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * ref->nSamples);
-	fftw_plan p = fftw_plan_dft_r2c_1d(ref->nSamples, samplesD, out,
-			FFTW_ESTIMATE);
-	fftw_execute(p);
-	fftw_destroy_plan(p);
-	fftw_free(out);
+	WhaleSoundRecognition w;
+	w.Init("/home/michael/Documents/Kaggle/Whale sounds/data/train/",
+			"/home/michael/Documents/Kaggle/Whale sounds/data/test/");
+	w.Train();
 	return 0;
 }
--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 CXXFLAGS =	-O2 -g -Wall -fmessage-length=0
 
-OBJS =		CaptainAhab.o
+OBJS =		CaptainAhab.o WhaleSoundRecognition.o FeatureVector.o
 
-CPPS =		CaptainAhab.cpp
+CPPS =		CaptainAhab.cpp WhaleSoundRecognition.cpp FeatureVector.cpp
 
-LIBPATH =	-L/usr/local/lib
+LIBPATH =	-L/usr/local/lib -L/usr/lib
 
-INCPATH =	-I/usr/local/include
+INCPATH =	-I/usr/local/include -I/usr/include
 
-LIBS =		-lfftw3 -lm -laiff
+LIBS =		-lfftw3 -lm -laiff -lboost_system -lboost_regex -lboost_filesystem -lpthread
 
 TARGET =	CaptainAhab
 
